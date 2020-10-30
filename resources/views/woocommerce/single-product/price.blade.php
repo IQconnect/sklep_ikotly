e7ffc3874daf4b9032924b5f71f0e61f36358e62
ralph <rafal@creativeroom.pl>
2020-10-30 15:11:18 +0100

    vat tabs

--- resources/views/woocommerce/single-product/price.blade.php
+++ resources/views/woocommerce/single-product/price.blade.php
@@ -32,7 +32,162 @@ global $product;
   $vat = $product->attributes['pa_vat']['options']
 @endphp
 
-@if($product->attributes)
+<ul class="nav nav-tabs" id="myTab" role="tablist">
+  <li class="nav-item" role="presentation">
+    <a class="nav-link active" id="vat-tab" data-toggle="tab" href="#vat" role="tab" aria-controls="vat" aria-selected="true">
+      VAT 8%
+    </a>
+  </li>
+  <li class="nav-item" role="presentation">
+    <a class="nav-link" id="vat2-tab" data-toggle="tab" href="#vat2" role="tab" aria-controls="vat2" aria-selected="false">
+      VAT 23%
+    </a>
+  </li>
+</ul>
+<div class="tab-content" id="myTabContent">
+  {{-- 8 % --}}
+  <div class="tab-pane fade show active" id="vat" role="tabpanel" aria-labelledby="vat-tab">
+    @if($product->attributes)
+      <table class="table-small mb-5">
+        <thead>
+          <tr>
+            @if($powers)
+              <th>
+                Moc
+              </th>
+            @endif
+            @if($liters)
+              <th>
+                Pojemność
+              </th>
+            @endif
+            {{-- @if($vat)
+              <th>
+                VAT
+              </th>
+            @endif --}}
+            <th>
+              Cena
+            </th>
+            <th>
+              Oblicz ratę
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach($product->get_available_variations() as $item)
+            @if($item['attributes']['attribute_pa_vat'] == "8")
+              <tr>
+                @if($powers)
+                  <td>
+                    {{ $item['attributes']['attribute_pa_moc'] }} kW
+                  </td>
+                @endif
+                @if($liters)
+                  <td>
+                    {{ $item['attributes']['attribute_pa_pojemnosc'] }} L
+                  </td>
+                @endif
+                {{-- @if($vat)
+                  <td>
+                    {{ $item['attributes']['attribute_pa_vat'] }} %
+                  </td>
+                @endif --}}
+                <td class="primary text--bold">
+                  <strong>
+                    {{ number_format($item['display_price'], 2, ',', '') }} zł
+                  </strong>
+                </td>
+                <td>
+                  <div class="table-small__icons">
+                    <a href="#" onClick="window.open('https://ewniosek.credit-agricole.pl/eWniosek/simulator.jsp?PARAM_TYPE=RAT&amp;PARAM_PROFILE=PSP2002238&amp;PARAM_CREDIT_AMOUNT={{ $item['display_price'] }}','MyWindow','width=820,height=630'); return false;">
+                      <img src="@asset('images/calogo.png')">
+                    </a>
+                    <a href="#" onClick="window.open('https://irata.bgzbnpparibas.pl/eshop-form/calc?RequestedAmount={{ $item['display_price'] }}&amp;AgreementNo=2202924&amp;CreditType=123465;100206;100254','MyWindow','width=820,height=630'); return false;">
+                      <img src="@asset('images/bnplogo.png')">
+                    </a>
+                  </div>
+                </td>
+              </tr>
+            @endif
+          @endforeach
+        </tbody>
+      </table>
+    @endif
+  </div>
+  {{-- 23 % --}}
+  <div class="tab-pane fade" id="vat2" role="tabpanel" aria-labelledby="vat2-tab">
+    @if($product->attributes)
+      <table class="table-small mb-5">
+        <thead>
+          <tr>
+            @if($powers)
+              <th>
+                Moc
+              </th>
+            @endif
+            @if($liters)
+              <th>
+                Pojemność
+              </th>
+            @endif
+            {{-- @if($vat)
+              <th>
+                VAT
+              </th>
+            @endif --}}
+            <th>
+              Cena
+            </th>
+            <th>
+              Oblicz ratę
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach($product->get_available_variations() as $item)
+            @if($item['attributes']['attribute_pa_vat'] == "23")
+              <tr>
+                @if($powers)
+                  <td>
+                    {{ $item['attributes']['attribute_pa_moc'] }} kW
+                  </td>
+                @endif
+                @if($liters)
+                  <td>
+                    {{ $item['attributes']['attribute_pa_pojemnosc'] }} L
+                  </td>
+                @endif
+                {{-- @if($vat)
+                  <td>
+                    {{ $item['attributes']['attribute_pa_vat'] }} %
+                  </td>
+                @endif --}}
+                <td class="primary text--bold">
+                  <strong>
+                    {{ number_format($item['display_price'], 2, ',', '') }} zł
+                  </strong>
+                </td>
+                <td>
+                  <div class="table-small__icons">
+                    <a href="#" onClick="window.open('https://ewniosek.credit-agricole.pl/eWniosek/simulator.jsp?PARAM_TYPE=RAT&amp;PARAM_PROFILE=PSP2002238&amp;PARAM_CREDIT_AMOUNT={{ $item['display_price'] }}','MyWindow','width=820,height=630'); return false;">
+                      <img src="@asset('images/calogo.png')">
+                    </a>
+                    <a href="#" onClick="window.open('https://irata.bgzbnpparibas.pl/eshop-form/calc?RequestedAmount={{ $item['display_price'] }}&amp;AgreementNo=2202924&amp;CreditType=123465;100206;100254','MyWindow','width=820,height=630'); return false;">
+                      <img src="@asset('images/bnplogo.png')">
+                    </a>
+                  </div>
+                </td>
+              </tr>
+            @endif
+          @endforeach
+        </tbody>
+      </table>
+    @endif
+  </div>
+</div>
+
+{{-- @if($product->attributes)
   <table class="table-small mb-5">
     <thead>
       <tr>
@@ -96,4 +251,4 @@ global $product;
       @endforeach
     </tbody>
   </table>
-@endif
+@endif --}}
